33da0a76a904d5f01c2df24fcf3a290be34660fa
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-03 07:37:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercu", age: "30", attitude: "confused"}
+         {name: "Mercutio", age: "30", attitude: "confused"}
       ]
    }, 
    :capulet => {
